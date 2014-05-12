c540613b443aa544ffe61f5c7b1d617303c07fa7
tlindener <tobias.lindener@gmail.com>
2014-05-12 14:11:04 +0200

    fixed client.sh

--- buildnodered.sh
+++ buildnodered.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/iot/iotframework/nodered
 docker build --rm -t tlindener/nodered .
-nodered=$(sudo docker run -d -P 1880:1880 --name nodered tlindener/nodered)
+nodered=$(sudo docker run -d -p 1880:1880 --name nodered tlindener/nodered)
 cd /home/iot/iotframework
 ./pipework br1 $nodered 192.168.5.4/24

2d107c89c6ffb84815dcac7092ab9f8413c59202
tlindener <tobias.lindener@gmail.com>
2014-05-14 19:06:48 +0200

    Fixed apache

--- apache/index.php
+++ apache/index.php
@@ -1,1 +1,44 @@
-<?php phpinfo(); ?>
+<?php
+
+$client = new Mosquitto\Client();
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onSubscribe('subscribe');
+$client->onMessage('message');
+$client->connect("192.168.5.5", 1883, 5);
+$client->onLog('logger');
+$client->subscribe('#', 1);
+
+for ($i = 0; $i < 10; $i++) {
+    $client->loop();
+}
+
+$client->unsubscribe('#');
+
+for ($i = 0; $i < 10; $i++) {
+    $client->loop();
+}
+
+function connect($r, $message) {
+echo "I got code {$r} and message {$message}\n";
+}
+
+function subscribe() {
+echo "Subscribed to a topic\n";
+}
+
+function unsubscribe() {
+echo "Unsubscribed from a topic\n";
+}
+
+function message($message) {
+printf("Got a message on topic %s with payload:\n%s\n", $message->topic, $message->payload);
+}
+
+function disconnect() {
+echo "Disconnected cleanly\n";
+}
+
+function logger() {
+var_dump(func_get_args());
+}

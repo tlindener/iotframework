3ac346d253c5d36af7c1157292efe5b8f103f951
tlindener <tobias.lindener@gmail.com>
2014-05-20 13:25:02 +0200

    changed script to link container with ovs container directly

--- buildmqttclients.sh
+++ buildmqttclients.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 cd /home/iot/iotframework/mqttclient
 docker build --rm -t tlindener/mqttclient .
-for i in {6..10}
-do
-	mqttserver=$(sudo docker run -d -P tlindener/mqttclient)
-	sudo docker inspect --format='{{ .State.Pid }}' $mqttserver 	
-done
-
 cd /home/iot/iotframework/mqttserver
 docker build --rm -t tlindener/mqttserver .
-mqttserver=$(sudo docker run -d -P tlindener/mqttserver)
-sudo docker inspect --format='{{ .State.Pid }}' $mqttserver
+
+
+ovs=$(docker run -p 6640:6640 -p 6633:6633 -p 6644:6644 --privileged=true -d -i -t davetucker/docker-ovs:2.1.2 /bin/supervisord -n)
+ovspid=$(docker inspect --format='{{ .State.Pid }}' $ovs)
+rm -f /var/run/netns/$ovspid
+ln -s /proc/$ovspid/ns/net /var/run/netns/$ovspid
+
+mqttserver=$(docker run -d -P tlindener/mqttserver)
+mqttserverpid=$(docker inspect --format='{{ .State.Pid }}' $mqttserver)
+rm -f /var/run/netns/$mqttserverpid
+ln -s /proc/$mqttserverpid/ns/net /var/run/netns/$mqttserverpid
+echo $mqttserverpid
+
+ovs-vsctl --db=tcp:172.17.42.1:6640 add-br br0
+ovs-vsctl --db=tcp:172.17.42.1:6640 set bridge br0 datapath_type=netdev
+
+switchcounter=1
+tmplinkname1=$("eth"$ovspid)
+tmplinkname2=$("eth"$mqttserverpid)
+ip link add name $tmplinkname1 type veth peer name $tmplinkname2
+ip link set $tmplinkname1 netns $ovspid
+ip link set $tmplinkname2 netns $mqttserverpid
+ip netns exec $mqttserverpid ip link set $tmplinkname2 name eth1
+ip netns exec $ovspid ip link set $tmplinkname1 name eth$switchcounter
+ovs-vsctl --db=tcp:172.17.42.1:6640 add-port br0 eth$switchcounter 
+ip netns exec $mqttserverpid ifconfig eth1 192.168.5.5/24 up
+ip netns exec $ovspid ip link set eth$switchcounter up
+switchcounter=$($switchcounter+1)
+
+for i in {6..10}
+do
+	mqttclient=$(sudo docker run -d -P tlindener/mqttclient)
+	dockerpid=$(docker inspect --format='{{ .State.Pid }}' $mqttserver)
+	rm -f /var/run/netns/$dockerpid
+	ln -s /proc/$dockerpid/ns/net /var/run/netns/$dockerpid
+	tmp=$(eth$dockerpid)
+	ip link add name $tmp type veth peer name local-veth1
+	ip link set $tmp netns $ovspid
+	ip link set local-veth1 netns $dockerpid
+	ip netns exec $dockerpid ip link set local-veth1 name eth1
+	ip netns exec $ovspid ip link set $tmp name eth$switchcounter
+	ovs-vsctl --db=tcp:172.17.42.1:6640 add-port br0 eth$switchcounter 
+	ip netns exec $dockerpid ifconfig eth1 192.168.5.$i/24 up
+	ip netns exec $ovspid ip link set eth$switchcounter up
+	switchcounter=$($switchcounter+1)
+done
+
+
+
+
+

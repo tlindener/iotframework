5776d427d82958323ef164534f0e2cb9547c2b77
tlindener <tobias.lindener@gmail.com>
2014-06-11 10:17:48 +0200

    Python script working

--- createNetwork.py
+++ createNetwork.py
@@ -6,18 +6,18 @@ import time
 ovs = DockerContainer("","davetucker/docker-ovs:2.1.2")
 ovs.create("/bin/supervisord -n",[6633, 6640, 6644])
 ovs.run({6633: 6633, 6640: 6640, 6644: 6644})
-time.sleep(10)
+time.sleep(5)
 switch = OpenVSwitch("tcp:172.17.42.1:6640")
 switch.addBridge("ovsbr0")
 
 container = DockerContainer("/home/iot/iotframework/mqttserver/","tlindener/mqttserver")
 container.build()
-container.create("/bin/supervisord -n",[22,1883])
+container.create("/usr/bin/supervisord",[22,1883])
 container.run({22: 5000, 1883: 1883})
 container.attachtonetwork(ovs.ContainerPid,"eth5","eth1","192.168.5.5/24")
 
 for a in range(6,10):
-	container2 = DockerContainer("mqttserver/","tlindener/mqttclient")
+	container2 = DockerContainer("/home/iot/iotframework/mqttclient/","tlindener/mqttclient")
 	container2.build()
 	container2.create("",[])
 	container2.run({})
